331c8b2ddd3f4789f46e9bfbcc25b382b0d3cbf0
sandeep <weguruchela@gmail.com>
2019-12-02 11:58:24 +0545

    Dashboard and login completed

--- app/src/main/java/com/example/drdesktop/MainActivity.java
+++ app/src/main/java/com/example/drdesktop/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.drdesktop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etUsername,etPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+
+
+
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etUsername.getText().toString().equals("admin") &&
+                        etPassword.getText().toString().equals("admin"))
+                {
+                    Intent intent=new Intent(MainActivity.this,
+                            DashboardActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
 }
